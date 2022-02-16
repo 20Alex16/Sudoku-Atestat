4364b83acaa16fde54db7a38264e6c4394f39535
20Alex16 <tripaa.alex@gmail.com>
2022-02-16 16:28:09 +0200

    Add project files.

---
+++ Sudoku Atestat/SudokuEngine.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sudoku_Atestat
+{
+    public class SudokuEngine
+    {
+        private Panel container;
+        private matrice9x9 mat;
+    }
+
+    public class matrice9x9
+    {
+
+    }
+
+    public class matriceSudoku9x9
+    {
+
+    }
+}
